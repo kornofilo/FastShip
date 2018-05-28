0c5b41e826ad16e169669f224cde77bec63af264
Logan8192 <wsses11000@gmail.com>
2018-05-28 16:10:02 -0500

    Agregado Rutas y Componente para footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { OperacionesComponent } from './operaciones/operaciones.component';
+import { RouterModule, Routes } from '@angular/router';
+
+/* Array de Rutas */
+const routes: Routes = [
+  {  path: 'operaciones', component: OperacionesComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    OperacionesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
+
 export class AppModule { }

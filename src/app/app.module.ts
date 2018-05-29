a54df832a44c01857ea69764757498e0c9b29694
Logan8192 <wsses11000@gmail.com>
2018-05-29 00:03:17 -0500

    Agregado componentes de Home y Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFireAuth } from 'angularfire2/auth';
+import { auth } from 'firebase/app';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { OperacionesComponent } from './operaciones/operaciones.component';
 import { RouterModule, Routes } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { UsuarioComponent } from './usuario/usuario.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 /* Array de Rutas */
 const routes: Routes = [
-  {  path: 'operaciones', component: OperacionesComponent }
+  {  path: 'home', component: HomeComponent },
+  {  path: 'login', component: LoginComponent },
+  {  path: 'operaciones', component: OperacionesComponent },
   {  path: 'usuario', component: UsuarioComponent }
 
 ];
@@ -21,7 +26,9 @@ const routes: Routes = [
     FooterComponent,
     OperacionesComponent,
     NavbarComponent,
-    UsuarioComponent
+    UsuarioComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,

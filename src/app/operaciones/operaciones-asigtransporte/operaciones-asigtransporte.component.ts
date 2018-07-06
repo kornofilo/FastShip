9376442bcf3570933b5d1dbb9023a74381ff72fb
Logan8192 <wsses11000@gmail.com>
2018-07-05 22:13:31 -0500

    Crear Cargamento - Fix del bug del Unsubscribe

--- src/app/operaciones/operaciones-asigtransporte/operaciones-asigtransporte.component.ts
+++ src/app/operaciones/operaciones-asigtransporte/operaciones-asigtransporte.component.ts
@@ -7,7 +7,7 @@ import { AngularFireAuth } from 'angularfire2/auth';
 import { auth } from 'firebase/app';
 import { AuthService } from '../../services/auth.service';
 import { FindValueSubscriber } from 'rxjs/internal/operators/find';
-import { Form,FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { Subscription } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
 import { FirestoreEnviosService } from '../../services/firestore-envios.service';
@@ -18,7 +18,7 @@ import { FirestoreTransportesEnvioService} from '../../services/firestore-transp
   styleUrls: ['./operaciones-asigtransporte.component.css']
 })
 export class OperacionesAsigtransporteComponent implements OnInit, OnDestroy, OnChanges {
-  arrTransporte:Transporte [] = [];
+  arrTransporte: Transporte [] = [];
   arr: Envios[] = [];
     arre: Envios[] = [];
     transporte: string;
@@ -34,8 +34,8 @@ private firebaseEnvioSubscription: Subscription;
 private firebaseDestinoSubscription: Subscription;
 private firestoreTransportesSubscription: Subscription;
 
-  constructor(public authService: AuthService ,public _misTransporte: FirestoreTransportesEnvioService,  public route: ActivatedRoute
-    , public _miDestino:  FirestoreEnviosService,private fb: FormBuilder
+  constructor(public authService: AuthService , public _misTransporte: FirestoreTransportesEnvioService,  public route: ActivatedRoute
+    , public _miDestino:  FirestoreEnviosService, private fb: FormBuilder
   ) {}
 
   ngOnInit() {
@@ -75,7 +75,6 @@ private firestoreTransportesSubscription: Subscription;
   // Finalizamos la suscripción con el servicio al cerrar el componente.
   ngOnDestroy() {
     this.firestoreTransportesSubscription.unsubscribe();
-    this.firebaseEnvioSubscription.unsubscribe();
     this.firebaseDestinoSubscription.unsubscribe();
   }
   ngOnChanges() {
@@ -106,21 +105,24 @@ this.crearTransporteform.reset();
 
   updateSubmit() {
 
-    //Actualizamos el destino del transporte.
+    // Actualizamos el destino del transporte.
     this._misTransporte.updateTransporte(this.iME, this.crearTransporteform.value);
 
-    this.firebaseEnvioSubscription = this._miDestino.getEnviosByOrigAndDest(this.transporte,this.crearTransporteform.get('destinoAsig').value).subscribe(
+    this.firebaseEnvioSubscription = this._miDestino.getEnviosByOrigAndDest(this.transporte,
+      this.crearTransporteform.get('destinoAsig').value).subscribe(
       (envio: Envios[]) => {
       this.arr = envio;
       console.log(this.arr);
-      let newEH = {
+      const newEH = {
         estado: 'Cargado',
         fecha: Date.now()
-      }
+      };
+
       this.arr.forEach( (element) => {
-          console.log(element.id,newEH,this.placa);
+          console.log(element.id, newEH, this.placa);
           this._miDestino.updateEstadoEnvio(element.id, newEH);
-          this._miDestino.asignarTrasporte(element.id,this.placa);
+          this._miDestino.asignarTrasporte(element.id, this.placa);
+          this.firebaseEnvioSubscription.unsubscribe();
         });
     });
     this.cleanForm();

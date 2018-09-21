acacf29afa7e5d3095661d6df406f3ad3dae0891
Logan8192 <wsses11000@gmail.com>
2018-09-20 22:13:11 -0500

    Mejoras en el diseño del formulario de login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,8 +1,6 @@
-import { Component, OnInit, Injectable } from '@angular/core';
-import { AngularFireAuth } from 'angularfire2/auth';
-import * as firebase from 'firebase/app';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
-import {NgForm} from '@angular/forms';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 
 @Component({
@@ -12,26 +10,37 @@ import {NgForm} from '@angular/forms';
 })
 
 export class LoginComponent implements OnInit {
-  constructor(public authService: AuthService) {}
-  public email: string;
-  public password: string;
+  constructor(public authService: AuthService, private fb: FormBuilder) {}  
+  loginForm: FormGroup;
 
   ngOnInit() {
+    this.createForm();
   }
 
-  onSubmitLogin() {
-    this.authService.login(this.email, this.password)
+  //Función que se encarga de llamar al servicio de autenticación de Firebase para validar las credenciales ingresadas por el usuario.
+  onSubmit() {
+    this.authService.login(this.loginForm.get('email').value, this.loginForm.get('password').value)      
     .then(res => {
       console.log(res);
+      //Si las credenciales son correctas, se redirige al usuario al componente de operaciones.
       location.replace('operaciones');
     }).catch( (err) => {
-      if (err.code === 'auth/wrong-password') {
+      // Gestión de errores de autenticación.
+      if (err.code === 'auth/wrong-password') { //Si la contraseña ingresada es incorrecta.
         alert('El Nombre de Usuario o Contraseña ingresada son incorrectos. Inténtelo de nuevo.');
-      } else if (err.code === 'auth/invalid-email') {
+      } else if (err.code === 'auth/invalid-email') { //Si el formato del email ingresado es incorrecto.
         alert('El formato del correo electrónico ingresado es inválido. Inténtelo de nuevo.');
-      } else if (err.code === 'auth/user-not-found') {
+      } else if (err.code === 'auth/user-not-found') { //Si el email ingresado no se encuentra segistrado en el sistema.
         alert('El correo ingresado no se encuentra registrado en el sistema. Inténtelo de nuevo.');
       }
+    });
+  }
+
+  // Función para la creación del FormGroup que contiene las credenciales de inicio de sesión.
+  createForm() {
+    this.loginForm = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],      
     });
   }
 }
